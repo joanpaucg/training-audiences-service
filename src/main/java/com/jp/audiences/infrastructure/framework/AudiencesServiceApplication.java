d1077a1fe744fb59b1f46f95d76c39528c1a3ffa
JoanPau <joanpau.castells@hotmail.com>
2025-02-26 01:05:11 +0100

    Add infrastructure repository in order to connect to spring jpa

--- src/main/java/com/jp/audiences/infrastructure/framework/AudiencesServiceApplication.java
+++ src/main/java/com/jp/audiences/infrastructure/framework/AudiencesServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.jp.audiences")
-
+@EnableJpaRepositories
 public class AudiencesServiceApplication {
 
 	public static void main(String[] args) {
